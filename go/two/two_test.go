c3e7f8259dce06bda7ddc2576edac87c8b17e757
Rob Martin <rob.martin@axios.com>
2022-12-03 15:11:35 -0700

    fix out of date function names

--- go/two/two_test.go
+++ go/two/two_test.go
@@ -12,17 +12,17 @@ func testData() []string {
 	}
 }
 
-func TestRPS(t *testing.T) {
+func TestRockPaperScissors(t *testing.T) {
 	expect := 15
-	got := RPS(testData())
+	got := RockPaperScissors(testData())
 	if got != expect {
 		t.Errorf("Expected %d, got %d", expect, got)
 	}
 }
 
-func TestStrategicRPS(t *testing.T) {
+func TestStrategicRockPaperScissors(t *testing.T) {
 	expect := 12
-	got := StrategicRPS(testData())
+	got := StrategicRockPaperScissors(testData())
 	if got != expect {
 		t.Errorf("Expected %d, got %d", expect, got)
 	}
